f282041b3af16db99b2c8ae655c02b08b7ff3400
Jharna203 <jharnakhatun203@gmail.com>
2023-03-25 15:57:25 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,20 +9,72 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send(`Welcome to Server `);
+app.get("/", (req, res) => {
+  res.send(`Welcome to Server `);
 });
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.9b1wrmq.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    //collection with mongodb
+    const serviceCollection = client.db("carServices").collection("service");
+    const orderCollection = client.db("carServices").collection("orders");
 
-app.listen(port, ()=>{
-    console.log(`Listening to port ${port}`)
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //orders api 
+    app.get("/orders", async (req, res) => {
+      let query = {};
+
+      if(req.query.email){
+        query = { 
+          email: req.query.email
+        }
+      }
+
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.post('/orders',async(req, res)=>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+
+    app.delete('/orders/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)};
+        const result = await orderCollection.deleteOne(query);
+        res.send(result);
+    })
+
+  } 
+  finally {
+
+  }
+}
+run().catch((err) => console.error(err));
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 });
